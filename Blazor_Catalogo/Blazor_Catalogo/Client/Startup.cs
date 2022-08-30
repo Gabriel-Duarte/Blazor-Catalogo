a88619be264b8a7ff58fbdba86a08b620f1aa742
Gabriel Duarte <56050731+biel16@users.noreply.github.com>
2022-08-30 16:43:05 -0300

    initial commit

---
+++ Blazor_Catalogo/Blazor_Catalogo/Client/Startup.cs
@@ -0,0 +1,29 @@
+using Blazor_Catalogo.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor_Catalogo.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorizationCore();
+
+            services.AddScoped<TokenAuthenticationProvider>();
+
+            services.AddScoped<IAuthorizeService, TokenAuthenticationProvider>(
+                provider => provider.GetRequiredService<TokenAuthenticationProvider>()
+                );
+
+            services.AddScoped<AuthenticationStateProvider, TokenAuthenticationProvider>(
+              provider => provider.GetRequiredService<TokenAuthenticationProvider>());
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
