bd028990419626934bba386d8bc8a90bbf4c39c3
Gabriel Duarte <gabriel.sobrinho@elis.com>
2022-11-17 15:22:29 -0300

    Feat: Migraçao para o NetFramework 6

--- Blazor_Admin/Blazor_Admin/Blazor_Admin/Program.cs
+++ Blazor_Admin/Blazor_Admin/Blazor_Admin/Program.cs
@@ -1,28 +1,56 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using Blazor_Admin.Areas.Identity;
+using Blazor_Admin.Data;
+using Blazor_Admin.Data.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Blazor_Admin
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.  =>  Equivale ao método ConfigureServices
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var sqlConnectionConfiguration = new SqlConnectionConfiguration(
+               builder.Configuration.GetConnectionString("SqlDbContext"));
+
+builder.Services.AddSingleton(sqlConnectionConfiguration);
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+                 options.SignIn.RequireConfirmedAccount = true)
+                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<AuthenticationStateProvider,
+     RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. Equivale ao método Configure()
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
